7e70e377cde05d8dab5791d382ab7ab8f9150c59
Ruby Berumen <clararubipb15@gmail.com>
2021-11-28 18:26:28 -0600

    Se agregaron los metodos pata el abcc en proyectoDAO

--- app/src/main/java/controladoresDAO/ProyectoDAO.java
+++ app/src/main/java/controladoresDAO/ProyectoDAO.java
@@ -1,5 +1,36 @@
 package controladoresDAO;
 
-public class ProyectoDAO {
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import entidades.Proyecto;
+
+@Dao
+public interface ProyectoDAO {
+
+    //Altas
+    @Insert
+    public void insertarProyecto(Proyecto proyecto);
+
+    //Bajas
+    @Query("DELETE FROM Proyecto WHERE numProyecto= :nump")
+    public void eliminarProyecto(int nump);
+
+    //Cambios
+    @Query("UPDATE Proyecto SET nombre=:n, ubicacion=:u, numDepartamento=:nd WHERE numProyecto=:nump")
+    public void modificarProyecto(int nump, String n, String u, Byte nd);
+
+    //Consultas
+    @Query("SELECT * FROM Proyecto")
+    public List<Proyecto> obtenerTodos();
+
+    @Query("SELECT * FROM Proyecto WHERE nombre LIKE :n")
+    public Proyecto buscarPorNombre(String n);
+
+    @Query("SELECT * FROM Proyecto WHERE numProyecto LIKE :nump")
+    public Proyecto buscarPorNumProyecto(int nump);
 
 }
--- app/src/main/java/entidades/Proyecto.java
+++ app/src/main/java/entidades/Proyecto.java
@@ -1,5 +1,60 @@
 package entidades;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Proyecto {
 
+    @NonNull
+    @ColumnInfo(name="nombre")
+    private String nombreProyecto;
+
+    @NonNull
+    @PrimaryKey
+    private int numProyecto;
+
+    @NonNull
+    @ColumnInfo(name="ubicacion")
+    private String ubicaciónProyecto;
+
+    @NonNull
+    @ColumnInfo(name="numDepartamento")
+    private byte numDptoProyecto;
+
+    @NonNull
+    public String getNombreProyecto() {
+        return nombreProyecto;
+    }
+
+    public void setNombreProyecto(@NonNull String nombreProyecto) {
+        this.nombreProyecto = nombreProyecto;
+    }
+
+    public int getNumProyecto() {
+        return numProyecto;
+    }
+
+    public void setNumProyecto(int numProyecto) {
+        this.numProyecto = numProyecto;
+    }
+
+    @NonNull
+    public String getUbicaciónProyecto() {
+        return ubicaciónProyecto;
+    }
+
+    public void setUbicaciónProyecto(@NonNull String ubicaciónProyecto) {
+        this.ubicaciónProyecto = ubicaciónProyecto;
+    }
+
+    public byte getNumDptoProyecto() {
+        return numDptoProyecto;
+    }
+
+    public void setNumDptoProyecto(byte numDptoProyecto) {
+        this.numDptoProyecto = numDptoProyecto;
+    }
 }
